31855150f1e10cee0e00324bde63d677597e95b3
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2026-07-29 11:26:21 +0500

    roles

---
+++ src/routes/roleRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const router = express.Router();
+
+const { getRoles } = require("../../src/controllers/roleController");
+
+const verify = require("../../src/middleware/verifyToken");
+
+router.get("/roles", verify, getRoles);
+
+module.exports = router;
