eb2f5bcb15c90d731de0999ad36e34cc5f9a8e0f
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2026-07-30 11:56:06 +0500

    before

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,8 @@
 const { initializeApp, cert, getApps, getApp } = require("firebase-admin/app");
-const keyPath = path.resolve(__dirname, '../../serviceAccountKey.json');
 const fs = require('fs');
 const path = require('path');
+
+const keyPath = path.resolve(__dirname, '../../serviceAccountKey.json');
 
 const serviceAccount = JSON.parse(
   fs.readFileSync(keyPath, 'utf8').replace(/^\uFEFF/, '') 
